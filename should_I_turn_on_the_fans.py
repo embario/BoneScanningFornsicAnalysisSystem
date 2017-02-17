0eedd0f0021894f92ec02e152310a05500c80e26
Anshul Kanakia <akanakia@users.noreply.github.com>
2017-02-17 08:02:30 -0800

    Adding fan-turn-on logic

---
+++ should_I_turn_on_the_fans.py
@@ -0,0 +1,8 @@
+def should_I_turn_on_the_fans(curr_temp):
+	turn_on_fans = False
+	# I really hope no MALWARE changes the number in this file...
+	with open("fan_temp_const.txt", 'r') as f:
+		read_temp = f.readline()
+	if curr_temp >= int(read_temp)
+		turn_on_fans = True
+	return turn_on_fans
